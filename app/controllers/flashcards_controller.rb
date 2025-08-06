35aa1763fff2628bbb24d12d43dfab232fa697c9
Benjo <maglicbenjamin@gmail.com>
2025-08-06 11:55:09 +0200

    images can now be removed from flashcards

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,19 +1,16 @@
 class FlashcardsController < ApplicationController
   before_action :set_flashcard, only: %i[ show update destroy ]
 
-  # GET /flashcards
   def index
     @flashcards = Flashcard.all
 
     render json: @flashcards
   end
 
-  # GET /flashcards/1
   def show
     render json: @flashcard, methods: [ :front_image_url, :back_image_url ]
   end
 
-  # POST /flashcards
   def create
     @flashcard = Flashcard.new(flashcard_params)
 
@@ -24,28 +21,27 @@ class FlashcardsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /flashcards/1
   def update
-    if @flashcard.update(flashcard_params)
+    @flashcard.front_image.purge if @flashcard.front_image.attached? && flashcard_params[:remove_front_image] == "true"
+    @flashcard.back_image.purge if @flashcard.back_image.attached? && flashcard_params[:remove_back_image] == "true"
+
+    if @flashcard.update(flashcard_params.except(:remove_front_image, :remove_back_image))
       render json: @flashcard, methods: [ :front_image_url, :back_image_url ]
     else
       render json: @flashcard.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /flashcards/1
   def destroy
     @flashcard.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_flashcard
       @flashcard = Flashcard.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def flashcard_params
-      params.expect(flashcard: [ :front, :back, :deck_id, :front_image, :back_image ])
+      params.expect(flashcard: [ :front, :back, :deck_id, :front_image, :back_image, :remove_front_image, :remove_back_image ])
     end
 end
