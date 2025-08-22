a804f5ece1d703c0adc9c79dd56027c01f4e98cd
Benjo <maglicbenjamin@gmail.com>
2025-08-22 12:41:27 +0200

    add rack-cors

--- app/misc/rodauth_main.rb
+++ app/misc/rodauth_main.rb
@@ -154,6 +154,7 @@ class RodauthMain < Rodauth::Rails::Auth
     # verify_account_grace_period 3.days.to_i
     # reset_password_deadline_interval Hash[hours: 6]
     # verify_login_change_deadline_interval Hash[days: 2]
+
     invalid_previous_password_message do
       "Wrong password"
     end
